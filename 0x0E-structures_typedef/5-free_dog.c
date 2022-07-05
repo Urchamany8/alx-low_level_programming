55955ac69e5f0300399efd1e549f526c6aa7c193
Nnenanya Uchenna <uchennajaphet@gmail.com>
2022-07-04 17:20:38 -0700

    a function that frees dogs.

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,33 @@
+s (15 sloc)  206 Bytes
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees memory allocated for a struct dog
+ * @d: struct dog
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+s (15 sloc)  206 Bytes
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees memory allocated for a struct dog
+ * @d: struct dog
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}}
